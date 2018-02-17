d4b5f05329b99cf9f36b3e2b17bb666b599c17a8
adubbya <aaron.mitchell@austin.utexas.edu>
2018-02-17 00:12:20 -0600

    spotify ist gut, still need fs, then twitter twittling

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+// GLOBAL VARS/REQUIRES
 //require dotenv
 var dotenv = require("dotenv").config();
 // require keys.js file
@@ -12,44 +13,51 @@ var Twitter = require("twitter");
 var Spotify = require('node-spotify-api');
 // require fs
 var fs = require("fs");
-
 //request keys
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-
-//first arg
+//nodefunc is the function the user wants to use, nodeinput is the searchterm or keyword to query
 var nodefunc = process.argv[2];
 var nodeinput = process.argv[3];
 
+
+// FUNCTION CALLS
 // node func selector switchcase
 function nodefuncSelector() {
     switch (nodefunc) {
         case "movie-this":
             omdbGET();
             break;
-    
+
         case "my-tweets":
-                tweetGET();
-                break;
+            tweetGET();
+            break;
 
         case "spotify-this-song":
-                songGET();
-                break;
+            songGET();
+            break;
+
+        case "do-what-it-says":
+            randoGET();
+            break;
+
         default:
-            console.log("not a function, please choose from:" + 
-            "\nmovie-this, spotify-this-song, my-tweets, do-what-it-says");
+            console.log("not a function, please choose from:" +
+                "\nmovie-this, spotify-this-song, my-tweets, do-what-it-says");
             break;
     }
 }
 
 
+// FUNCTION BLOCKS
 
-// ajax was getting hairy, chose this nifty library, hope is ok..
+// movie-this OMDB API  ajax was getting hairy, chose this nifty library, hope is ok..
 var omdbGET = function () {
     var params = {
         apiKey: "trilogy",
         title: nodeinput,
     }
+
     omdbc.get(params, function (err, data) {
         if (!err) {
             console.log("Movie Title: " + data.Title
@@ -62,32 +70,43 @@ var omdbGET = function () {
                 + "\nActors: " + data.Actors
                 + "\n===================\n");
         }
-        else {
+        else if (nodeinput === undefined) {
             console.log(err, "If you haven't watched Mr Nobody, then you should: http://www.imdb.com/title/tt0485947/, It's on Netflix!")
         }
     });
 };
 
-// my-tweets twitter API function, still figuring out how to parse this chunkodata: FOR LOOP
+// my-tweets Twitter API function, still figuring out how to parse this chunkodata: FOR LOOP
 var tweetGET = function () {
-    var params = {screen_name: 'UTAustin'};
-    client.get('statuses/user_timeline', params, function(error, tweet, response) {
+    var params = { screen_name: 'UTAustin' };
+    client.get('statuses/user_timeline', params, function (error, tweet, response) {
         if (!error) {
             console.log(tweet);
         }
     });
 }
 
-// spotify-this-song spotify API function, yeesh
+// spotify-this-song Spotify API function, yeesh, thinking usage with promises
 var songGET = function () {
-    spotify.search({ type: 'track', query: nodeinput }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        }
-       
-      console.log(data.); 
-      });
-}      
+
+    if (nodeinput === undefined) {// this is argh, wrong input???
+        nodeinput = "The Sign";
+    }
+
+    spotify.search({ type: 'track', query: nodeinput })
+        .then(function (response) {
+            console.log(   // this looks wet, stuff for later, but works!
+                "Artist Name: " + response.tracks.items[0].artists[0].name +
+                "\nSong Name: " + response.tracks.items[0].name +
+                "\nAlbum Name: " + response.tracks.items[0].album.name +
+                "\nSong URL: " + response.tracks.items[0].preview_url
+            );
+        })
+
+        .catch(function (err) {
+            console.log(err);
+        });
+}
 
 // do-what-it-says function
 
